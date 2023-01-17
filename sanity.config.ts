37265cb0c51da88e875e3a8582424d39db9a3efb
KenSison <ksison1991@gmail.com>
2023-01-17 14:36:54 +0900

    add studio page

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,15 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './schemas';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
   basePath: '/studio',
-  name: 'default',
-  title: 'box-pressed',
+  name: 'box-pressed-magazine',
+  title: 'box-pressed-magazine',
   projectId,
   dataset,
 
@@ -18,4 +18,4 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
-})
+});
