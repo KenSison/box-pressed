1bec0a0bf9f9c7d78cb0c69c5d086d7e784655c2
KenSison <ksison1991@gmail.com>
2023-01-17 14:55:00 +0900

    add theme skin for studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,8 @@ import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemas';
 
+import { myTheme } from './theme';
+
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
@@ -18,4 +20,6 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
+
+  theme: myTheme,
 });
